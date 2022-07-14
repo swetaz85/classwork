90330e2ad229f38ff8f29472978fe28edebf3ee0
Web <web@it.step>
2022-07-14 21:27:28 +0300

    add task2 classwork

--- loops/index.js
+++ loops/index.js
@@ -10,7 +10,62 @@
 // console.log('Hello');
 // } while (i > 5) 
 
-let i = 0;
-do {
-  i = +prompt('введите число');
-} while (i !== 5) 
+// let i = 0;
+// do {
+//   i = +prompt('введите число');
+// } while (i !== 5) 
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// for (let i; i !== 5; ) {
+//     i = +prompt('введите число');
+// }
+
+// for (let i = 0; i <= 10 ; i++) {
+//     // if (i !== 4) {
+//        if (i % 2 === 0) {
+//         console.log(i);
+//     }        
+// }
+
+// for (let i = 0; i <= 10 ; i++) {
+//      if (i === 4) {     
+//         continue;
+//     }        
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 10 ; i++) {
+//      if (i === 4) {     
+//         break;
+//     }        
+//     console.log(i);
+// }
+
+// for (let i = 1; i < 10 ; i++) {
+//     for (let j = 1; j < 5; j++) {
+//        console.log(`${i}.${j}`);        
+//     }
+// }
+
+// for (let i = 1; i <= 5 ; i++) {
+//     if (i === 3) {
+//        continue;
+//     }
+//     for (let j = 1; j < 5; j++) {
+//         if (j === 3) { // if (i === 3 || j === 3) 
+//          continue;
+//         }
+//        console.log(`${i}.${j}`);        
+//     }
+// }
+
+// for (let i = 10; i <= 99; i++) {
+//     if (i % 4 === 0 && i % 6 !== 0) {
+//         console.log(i);
+//     }    
+// }
+
+
