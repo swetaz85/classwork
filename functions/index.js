4accb7d89e2e61f08e36fbd1da2b2f5cf3d2c3c1
Web <web@it.step>
2022-08-04 21:19:25 +0300

    add task functions

--- functions/index.js
+++ functions/index.js
@@ -92,9 +92,7 @@
 // average(54,3564,54,54,5545,4,554);
 
 // let result = 0;
-// function average() { 
-//     //console.log(argumentsArr);    
-//     const argumentsArr = [...arguments];
+// function average(...argumentsArr) { 
 //     console.log(argumentsArr);
 //     for (let i = 0; i < argumentsArr.length; i++) {
 //         result += argumentsArr[i];        
@@ -102,3 +100,70 @@
 //     console.log(result / argumentsArr.length);
 // }
 // average(54,3564,54,54,5545,4,554);
+
+
+
+// hoisting - всплытие
+// console.log(a);
+// a = 2;
+
+// foo(); // найдет функцию
+// bar(); // ошибка, temporary dead zone (мертвая зона)
+// function foo () {
+//     console.log("Im foo");
+// }
+// foo = 2; // переприсвоится foo на переменную, функция исчезнет
+// console.log(foo);
+// const bar = function () {
+//     console.log("Im bar");
+// }
+// bar()
+
+// self invoking function (IIFE) - самовызывающаяся фунцкия
+// (function () { //anonymous - можно без названия
+//     console.log("Im foo");
+// }) ();
+
+
+// foo(); // найдет функцию
+// function foo () {
+//     console.log("Im foo");
+// }
+// foo();
+// function foo () {
+//     console.log("foo");
+// }
+// foo();
+// let bar = function () {
+//     console.log("Im bar");
+// }
+// bar();
+// bar = function () {
+//     console.log("bar");
+// }
+// bar();
+
+// block SCOPE
+// let a = 2;
+// var b = 3;
+// if (true) {
+//     let a = 4; // не перезапишет
+//     var b = 5; // перезапишет, у var нет блочной области видимости
+// }
+// console.log(a);
+// console.log(b);
+
+//function SCOPE, 
+// let a = 2;
+// function foo() {
+//     let a = 3; 
+//     console.log(a);
+//     function bar() {
+//         let a = 4; // если напишем a = 3, то вышерасположенная переменная a переприсвоится и в последней консоле ее выведет
+//         console.log(a);
+//     }
+//     bar();
+// }
+// foo();
+// console.log(a); // выведет 3, 4, 2
+
