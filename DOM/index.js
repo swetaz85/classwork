182106bd4064ca71b245f39ecbb91e8c22b2be92
Svetlana <swetaz@yandex.ru>
2022-09-27 21:18:37 +0300

    add promies end

---
+++ DOM/index.js
@@ -0,0 +1,7 @@
+// Document
+// Object
+// Model
+// console.dir(document.body);
+// console.dir(document.children[0].children[1]);
+// console.dir(document.children[0].children[1].children[1]);
+document.bgColor = 'red';
